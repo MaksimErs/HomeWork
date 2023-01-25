531eaad89e1649d73431b0642d355a7394962baf
MaksimErs <eroken@mail.ru>
2023-01-25 19:33:57 +0300

    добавили дз семинар 3

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -1,8 +1,9 @@
 ﻿Console.WriteLine("Введи число");
 int number =int.Parse(Console.ReadLine());
-int count=-number;
+int count=1;
 while(count<=number)
 {
-    Console.WriteLine(count);
+    double resalt=Math.Pow(count, 2);
+    Console.WriteLine($"квадрат {resalt}" );
     count++;
 }
---
+++ задание семинар3/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа принимает пятизнаяное число и проверяет является ли оно палиндромом
+Console.WriteLine("введи пятизначное число");
+int n= int.Parse(Console.ReadLine());
+int a=n/10000;
+int b=n/1000%10;
+int c=n/10%10;
+int d=n%10;
+if(a==d&&b==c)
+{
+    Console.WriteLine("является полиндромом");
+}
+else
+{
+        Console.WriteLine(" не является полиндромом");
+
+}
