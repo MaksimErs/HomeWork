765007b9bee3c1b9644ad6d77d07a6733c878ee9
MaksimErs <eroken@mail.ru>
2023-01-25 20:32:46 +0300

    добавили дз3 к семинару 3

---
+++ задание3 семинар3/Program.cs
@@ -0,0 +1,11 @@
+﻿// принимаем число n  и выдаем таблицу кубов от 1 до n
+Console.WriteLine("введи числа");
+int n= int.Parse(Console.ReadLine());
+int count=1;
+while (count<=n)
+{
+    double resalt=Math.Pow(count,3);
+    Console.WriteLine($"куб числа {resalt}");
+    count++; 
+}
+
