0a4cdf4402a41c977e8e619bd7a1084c84abcf59
MaksimErs <eroken@mail.ru>
2023-02-04 22:28:28 +0300

    дз6

---
+++ сем6 дз1/Program.cs
@@ -0,0 +1,37 @@
+﻿// вводим восемь чисел, получаем сколько чисел больше нуля ввели
+int[] Mas()
+{
+    int[] res=new int[8];
+    for(int i=0; i<8; i++)
+    {
+        res[i]=int.Parse(Console.ReadLine());
+    } 
+    return res;
+}
+
+void Print(int[] col)
+{
+    int count=col.Length;
+    int pos=0;
+    while(pos<count)
+    {
+        Console.Write(col[pos]);
+        pos++;
+    }
+    Console.WriteLine();
+}
+
+int Big(int []frry)
+{
+    int count=0;
+    foreach ( int item in frry)
+    {
+        if( item>0) count++;
+    }
+    return count;
+}
+
+int[]ddd=Mas();
+Print(ddd);
+int result= Big(ddd);
+Console.WriteLine($"количество чисел больше нуля {result}");
---
+++ сем6 дз2/Program.cs
@@ -0,0 +1,38 @@
+﻿// введите коэффициенты прямых y=k1*x+b1 y=k2*x+b2 в следующем порядке k1 b1 k2 b2  
+double[] Mas()
+{
+    double[] res=new double[4];
+    for(int i=0; i<4; i++)
+    {
+        res[i]=double.Parse(Console.ReadLine());
+    } 
+    return res;
+}
+
+void Print(double[] col)
+{
+    int count=col.Length;
+    int pos=0;
+    while(pos<count)
+    {
+        Console.Write(col[pos]);
+        pos++;
+    }
+    Console.WriteLine();
+}
+
+void Resalt(double[] sol)
+{
+double k1=sol[0];
+double b1=sol[1];
+double k2=sol[2];
+double b2=sol[3];
+double x=(b2-b1)/(k1-k2);
+double y=k2*x+b2;
+Console.WriteLine(x);
+Console.WriteLine(y);
+}
+double[] resy=Mas();
+Print(resy);
+Resalt(resy);
+
